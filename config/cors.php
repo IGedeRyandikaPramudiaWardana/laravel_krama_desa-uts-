1bff977e36d4e883f3b201958e016972c66b7995
I Gede Ryandika Pramudia Wardana <163832559+IGedeRyandikaPramudiaWardana@users.noreply.github.com>
2025-12-01 23:22:48 +0800

    Modify CORS allowed_origins to accept all
    Updated allowed_origins to accept all origins and commented out specific origins.

--- config/cors.php
+++ config/cors.php
@@ -20,10 +20,11 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'https://laravelkramadesa-uts-production.up.railway.app',
-        // 'http://127.0.0.1:5173',
-        // 'http://localhost:3000',
+        '*'
+        // 'http://localhost:5173',
+        // 'https://laravelkramadesa-uts-production.up.railway.app',
+        // // 'http://127.0.0.1:5173',
+        // // 'http://localhost:3000',
     ],
 
     'allowed_origins_patterns' => [],
